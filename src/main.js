6b0a193c48f9c4e3ef4cbf4d90c49f783a42802a
Jing <1344372401@qq.com>
2022-03-16 15:49:12 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@/assets/css/common.css'
+import '@/assets/css/bootstrap.min.css'
+import '@/assets/icons/feather.css'
+
 Vue.config.productionTip = false
 
 new Vue({
