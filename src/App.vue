a8337ccd010ebf4500f211004d747e48c2c403da
Jing <1344372401@qq.com>
2022-04-14 00:49:15 +0800

    create

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div>
     <Header />
+    <Tree :data-source="msg" />
     <router-view></router-view>
-    <Footer />
+    <Footer :msg="msg" />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Tree from '@/components/Tree'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer,
+    Tree
+  },
+  data () {
+    return {
+      msg: [
+        {
+          id: 'a-1',
+          title: "一级标题A",
+          next: [{
+            id: 'b-1',
+            title: "二级标题B",
+            next: [
+              {
+                id: 'c-1',
+                title: "三级标题C1",
+              },
+              {
+                id: 'c-2',
+                title: "三级标题C5",
+              }
+            ]
+          }]
+        },
+        {
+          id: 'a-2',
+          title: "一级标题B",
+          next: [{
+            id: 'b-1',
+            title: "二级标题A",
+            next: [{
+              id: 'c-1',
+              title: "三级标题C",
+            }]
+          }]
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Tree/index.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+  <div class="tree-wrap">
+    <div v-for="item in dataSource" :key="item.id">
+      <tree-item :list="item"></tree-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TreeItem from "./TreeItem"
+export default {
+  name: 'Tree',
+  props: {
+    dataSource: {
+      type: [Object, Array],
+      required: true
+    }
+  },
+  components: {
+    TreeItem
+  },
+};
+</script>
+<style scoped>
+.tree-wrap {
+  width: 100%;
+  /* padding: 0 50px; */
+}
+</style>
+
+
